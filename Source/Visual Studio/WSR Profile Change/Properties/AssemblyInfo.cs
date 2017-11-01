083a736ae2213fdb445a0f8e94ad6544145acc40
Exergist <31357974+Exergist@users.noreply.github.com>
2017-11-01 13:53:06 -0500

    Update AssemblyInfo.cs

--- Source/Visual Studio/WSR Profile Change/Properties/AssemblyInfo.cs
+++ Source/Visual Studio/WSR Profile Change/Properties/AssemblyInfo.cs
@@ -1,15 +1,15 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WSR Profile Change")]
+[assembly: AssemblyTitle("Change WSR Profile")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("WSR Profile Change")]
+[assembly: AssemblyProduct("Change WSR Profile")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b6b6a5cf-cf26-448b-8716-14fec3fd18f8")]
+[assembly: Guid("a31728dd-9f4a-41c6-8b32-ed1f74353623")]
 
 // Version information for an assembly consists of the following four values:
 //
